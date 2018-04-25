75c105a7ee053364055ff387d8f446935807b504
kzfelder <35270718+kzfelder@users.noreply.github.com>
2018-04-25 01:25:10 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.io.File;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("\n\nHello World.\n");
+
+        File awardRecipients = new File("turingAwards.txt");
+    }
+}
